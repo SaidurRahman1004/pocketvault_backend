5f49181d0eaf1a1fe42add0e0c193f6e6aaee2e7
siyamcst <saidurrahman1004@gmail.com>
2026-01-22 22:14:15 +0600

    View

--- api/views.py
+++ api/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+from .serializers import UserSerializer, ShoppingItemSerializer, BookmarkSerializer, MediaItemSerializer
+from .models import ShoppingItem, Bookmark, MediaItem
+#User ViewSet For User Registration And Info
+class UserCreate(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]  # Allow anyone to create  user
 
-# Create your views here.
+#ShoppingItem ViewSet
+class ShoppingItemViewSet(viewsets.ModelViewSet):
+    queryset = ShoppingItem.objects.all()
+    serializer_class = ShoppingItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    #Show only items that belong to the logged-in user //only login user Item shown
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+    
+    #when new item is created, set the current user(logged in user) automatically as the owner of the item 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+
+#Bookmark ViewSet
+class BookmarkViewSet(viewsets.ModelViewSet):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    #Show only items that belong to the logged-in user //only login user Item shown
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+    
+    #when new item is created, set the current user(logged in user) automatically as the owner of the item 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+#MediaItem ViewSet
+class MediaItemViewSet(viewsets.ModelViewSet):
+    queryset = MediaItem.objects.all()
+    serializer_class = MediaItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    #Show only items that belong to the logged-in user //only login user Item shown
+    def get_queryset(self):
+        return self.queryset.filter(user =self.request.user)
+    #when new item is created, set the current user(logged in user) automatically as the owner of the item 
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
