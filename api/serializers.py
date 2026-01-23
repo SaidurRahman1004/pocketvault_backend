ec6a71fee3a7ea731fb62eae4dab11a9fbdf515e
siyamcst <saidurrahman1004@gmail.com>
2026-01-23 11:17:51 +0600

    Password Feild Not Provided in serializer Fix

--- api/serializers.py
+++ api/serializers.py
@@ -5,12 +5,13 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
         #Create User With hash paswod
     def create(self, validated_data):
         user = User.objects.create_user(
             username= validated_data['username'],
+            email= validated_data.get('email', ""),
             password= validated_data['password']
         )
         return user
